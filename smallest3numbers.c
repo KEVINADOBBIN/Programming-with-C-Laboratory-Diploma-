48c03f62e7043c6cc50e342d0aed2fc8a2c084f7
Kevin A Dobbin <94518887+KEVINADOBBIN@users.noreply.github.com>
2025-12-25 11:29:56 +0530

    Refactor smallest3numbers.c for better readability
    Refactored the smallest number program to use standard main function and improved formatting.

--- smallest3numbers.c
+++ smallest3numbers.c
@@ -1,16 +1,20 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
+#include <stdio.h>
+int main()
 {
-int a,b,c;
-clrscr();
-printf("Enter 3 numbers: \n");
-scanf("%d%d%d",&a,&b,&c);
-If(a<=b && a<=c)
-printf("\n Smallest number = %d",a);
-else if(b>=a&& b>=c)
-printf("\n Smallest number = %d",b);
-else
-printf("\n Smallest number = %d",c);
-getch();
+    int a, b, c;
+    printf("Enter 3 numbers: \n");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a <= b && a <= c)
+    {
+        printf("\nSmallest number = %d\n", a);
+    }
+    else if (b <= a && b <= c)
+    {
+        printf("\nSmallest number = %d\n", b);
+    }
+    else
+    {
+        printf("\nSmallest number = %d\n", c);
+    }
+    return 0;
 }
